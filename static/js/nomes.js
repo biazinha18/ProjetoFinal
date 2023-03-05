c9a096beead85e9c67f1b2eefdb003a38277a5a4
biazinha18 <biaamada17@gmail.com>
2023-03-05 16:28:59 -0300

    Adicionada verificacao de idade nula

--- static/js/nomes.js
+++ static/js/nomes.js
@@ -1,7 +1,11 @@
 function mostrar_idade(id){
     $.get('https://api.agify.io/?name='+id, function(data, status){
         const input = document.querySelector('input[id = ' + id + ']');
-        input.value = data['age']
+        if (data['age'] === null){
+            input.value = 'Não encontrado'
+        } else {
+            input.value = data['age']
+        }
         console.log('Input: '+input+', Idade: '+data['age'])
     })
 }
